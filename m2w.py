5ed92bb85f20601c3352be806b0d77766688c507
Erkhemee <ericwasepic127@outlook.com>
2026-07-23 22:42:07 +0800

    Update m2w.py

--- m2w.py
+++ m2w.py
@@ -6,7 +6,8 @@ How to use?
     import pyscript, asyncio
     data = asyncio.run(pyscript.fetch("https://raw.githubusercontent.com/Ericwasepic127/Ericwasepic127/refs/heads/main/m2w.py"))
     with open("m2w.py", "w") as file:
-        file.write(data)
+        if data.ok:
+           file.write(asyncio.run(data.text()))
 2. After load, import it
   * Import just as `import m2w`
 3. If it's on ...
@@ -21,12 +22,15 @@ from pyodide.ffi import create_proxy
 
 class Main:
     """Main thread only, it will crash in Worker"""
-    def __init__(self):
-      self.worker = js.document.querySelector('script[type="py"][terminal]').xworker
+    def __init__(self, id='script[type="py"][terminal]'):
+      self.worker = js.document.querySelector(id).xworker
       self.sendmsg = self.worker.postMessage
       self.getmsg = None
+      self.msgs = []
+      self.id = id
       def on_message(event):
           self.getmsg = event.data
+          self.msgs.append(event.data)
       self.worker.onmessage = create_proxy(on_message)
 
 class Worker:
@@ -35,6 +39,8 @@ class Worker:
       self.worker = js.self
       self.sendmsg = self.worker.postMessage
       self.getmsg = None
+      self.msgs = []
       def on_message(event):
           self.getmsg = event.data
+          self.msgs.append(event.data)
       self.worker.onmessage = create_proxy(on_message)
